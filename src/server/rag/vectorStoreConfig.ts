7b1856d6aa673efc9d5ab633eadadc7a07ebcfeb
Fillbert Halim <fhalim3-c@my.cityu.edu.hk>
2026-02-28 18:04:04 +0800

    Revert "rag-implementation"
    This reverts commit ee26c5ca772400b1841270a465c0aa6a7d51bbee.

--- src/server/rag/vectorStoreConfig.ts
+++ src/server/rag/vectorStoreConfig.ts
@@ -33,7 +33,7 @@ const config = {
   } as PoolConfig,
   tableName: "testlangchainjs",
   collectionTableName: "collections",
-  collectionName: "GROUND_TRUTH",
+  collectionName: "documents",
   columns: {
     idColumnName: "id",
     vectorColumnName: "vector",
@@ -49,23 +49,22 @@ const vectorStore = await PGVectorStore.initialize(embeddings, config);
 export default vectorStore;
 
 const document1: Document = {
-  pageContent: "Hans likes to eat pizza",
+  pageContent: "The powerhouse of the cell is the mitochondria",
   metadata: { source: "https://example.com" },
 };
 
 const document2: Document = {
-  pageContent: "Ryan likes to jerk off to anime",
+  pageContent: "Buildings are made out of brick",
   metadata: { source: "https://example.com" },
 };
 
 const document3: Document = {
-  pageContent: "Leon likes to eat pasta",
+  pageContent: "Mitochondria are made out of lipids",
   metadata: { source: "https://example.com" },
 };
 
 const document4: Document = {
-  pageContent:
-    "Mitochondria are made out of two membranes, an outer membrane and a highly folded inner membrane. The inner membrane contains proteins that are responsible for the production of ATP, the energy currency of the cell. The space between the two membranes is called the intermembrane space, and the space inside the inner membrane is called the mitochondrial matrix. The outer membrane is smooth and allows small molecules to pass through, while the inner membrane is impermeable to most molecules and contains transport proteins that regulate the movement of molecules in and out of the mitochondria. The inner membrane also contains enzymes that are involved in the electron transport chain, which is a series of reactions that generate ATP through oxidative phosphorylation.",
+  pageContent: "The 2024 Olympics are in Paris",
   metadata: { source: "https://example.com" },
 };
 
@@ -75,7 +74,7 @@ const ids = [uuidv4(), uuidv4(), uuidv4(), uuidv4()];
 
 await vectorStore.addDocuments(documents, { ids: ids });
 const results = await vectorStore.similaritySearch(
-  "What does Ryan like to do?",
+  "What are mitochondria made out of?",
   2,
 );
 console.log(results);
@@ -94,7 +93,7 @@ const response = await model.invoke([
     "system",
     "You are a helpful assistant that answers questions about the content of documents.",
   ],
-  ["user", "What does Ryan like to do?"],
+  ["user", "What are mitochondria made out of?"],
   ["assistant", "The following documents may contain relevant information"],
   ["assistant", results.map((result) => result.pageContent).join("\n")],
 ]);
